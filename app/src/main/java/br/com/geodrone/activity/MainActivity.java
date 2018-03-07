d81390e1c549d871071d177de23fb70a63c4e9ca
lucianoft <lucianoft@gmail.com>
2018-03-06 22:29:50 -0300

    alteracao para menu na parte abaixo

--- app/src/main/java/br/com/geodrone/activity/MainActivity.java
+++ app/src/main/java/br/com/geodrone/activity/MainActivity.java
@@ -2,6 +2,9 @@ package br.com.geodrone.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.StringRes;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -18,11 +21,13 @@ import br.com.geodrone.R;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
-    @BindView(R.id.toolbar) Toolbar toolbar;
-    @BindView(R.id.fab) FloatingActionButton fab;
-    @BindView(R.id.drawer_layout) DrawerLayout drawer;
+    @BindView(R.id.toolbarMain)
+    Toolbar toolbar;
+
+    @BindView(R.id.navigationMain)
+    BottomNavigationView bottomNavigationView;
 
 
     @Override
@@ -30,86 +35,31 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //Mostrar o botão
+        getSupportActionBar().setHomeButtonEnabled(true);      //Ativar o botão
 
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
+        super.onBackPressed();
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_item1:
+                Intent i = new Intent(this,CadastroImagemActivity.class);
+                startActivity(i);
+                break;
+            default:
+                break;
+        }
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            Intent i = new Intent(this,CadastroImagemActivity.class);
-            startActivity(i);
-
-        } else if (id == R.id.nav_gallery) {
-            Intent i = new Intent(this,MapsActivity.class);
-            startActivity(i);
-        } else if (id == R.id.nav_slideshow) {
-            Intent i = new Intent(this,AndroidCameraApiActivity.class);
-            startActivity(i);
-
-        } else if (id == R.id.nav_manage) {
-            Intent i = new Intent(this,RegistroPragasActivity.class);
-            startActivity(i);
-
-
-        } else if (id == R.id.nav_share) {
-            Intent i = new Intent(this,RegChuvasActivity.class);
-            startActivity(i);
-
-        } else if (id == R.id.nav_send) {
-            Intent i = new Intent(this,CadastroPluviosidadeActivity.class);
-            startActivity(i);
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
 }
